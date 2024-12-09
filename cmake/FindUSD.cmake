b6b77d7d45fea9e506b582fe85ba88936bdef74e
Marvin Gajek <Marvin.Gajek1@web.de>
2024-12-09 16:39:03 +0100

    modify pathes in cmake module

--- cmake/FindUSD.cmake
+++ cmake/FindUSD.cmake
@@ -1,56 +1,71 @@
 # FindUSD.cmake - Find USD package based on known file names and directory structure
 
-if(APPLE) 
-    set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/vcpkg_installed/arm64-osx")
-elseif(UNIX) 
-    set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/third-party/OpenUSD")
-elseif(WIN32) 
-    set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/third-party/OpenUSD")
-else()  
+if(APPLE)
+    if(CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
+        set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/vcpkg_installed/arm64-osx")
+    else()
+        set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/vcpkg_installed/x86_64-osx")
+    endif()
+elseif(UNIX)
+    if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+        set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/vcpkg_installed/x64-linux")
+    elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+        set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/vcpkg_installed/arm64-linux")
+    else()
+        message(FATAL_ERROR "Unsupported architecture for UNIX platform: ${CMAKE_SYSTEM_PROCESSOR}")
+    endif()
+elseif(WIN32)
+    if(CMAKE_SYSTEM_PROCESSOR STREQUAL "AMD64")
+        set(USD_SEARCH_DIR "${CMAKE_SOURCE_DIR}/vcpkg_installed/x64-windows")
+    else()
+        message(FATAL_ERROR "Unsupported architecture for Windows platform: ${CMAKE_SYSTEM_PROCESSOR}")
+    endif()
+else()
     message(STATUS "Unsupported platform")
 endif()
 
-# Check if the USD directory exists
 if(EXISTS "${USD_SEARCH_DIR}")
     message(STATUS "Searching for USD in: ${USD_SEARCH_DIR}")
 
+    # Handle dynamic and static libraries separately based on platform
     if(APPLE) 
-        file(GLOB USD_LIBRARIES 
-            ${USD_SEARCH_DIR}/lib/*.a
-            ${USD_SEARCH_DIR}/lib/*.dylib)
+        file(GLOB USD_STATIC_LIBRARIES ${USD_SEARCH_DIR}/lib/*.a)
+        file(GLOB USD_DYNAMIC_LIBRARIES ${USD_SEARCH_DIR}/lib/*.dylib)
     elseif(UNIX) 
-        file(GLOB USD_LIBRARIES 
-            ${USD_SEARCH_DIR}/lib/*.a
-            ${USD_SEARCH_DIR}/lib/*.so)
+        file(GLOB USD_STATIC_LIBRARIES ${USD_SEARCH_DIR}/lib/*.a)
+        file(GLOB USD_DYNAMIC_LIBRARIES ${USD_SEARCH_DIR}/bin/*.so)
     elseif(WIN32) 
-        file(GLOB USD_LIBRARIES 
-            ${USD_SEARCH_DIR}/lib/*.lib
-            ${USD_SEARCH_DIR}/lib/*.dll)
+        file(GLOB_RECURSE USD_STATIC_LIBRARIES ${USD_SEARCH_DIR}/lib/*.lib)
+        file(GLOB_RECURSE USD_DYNAMIC_LIBRARIES ${USD_SEARCH_DIR}/bin/*.dll)
     else()  
         message(STATUS "Unsupported platform")
     endif()
-    
-    if(USD_LIBRARIES)
-        message(STATUS "Found USD library: ${USD_LIBRARIES}")
-        set(USD_LIB_DIR "${USD_SEARCH_DIR}/lib")
+
+    if(USD_STATIC_LIBRARIES)
+        message(STATUS "Found static USD libraries: ${USD_STATIC_LIBRARIES}")
+        set(USD_STATIC_LIB_DIR "${USD_SEARCH_DIR}/lib")
     else()
-        message(FATAL_ERROR "USD library 'libusd_usd.dylib' not found in: ${USD_SEARCH_DIR}/lib")
+        message(FATAL_ERROR "Static USD libraries not found in: ${USD_SEARCH_DIR}/lib")
+    endif()
+
+    if(USD_DYNAMIC_LIBRARIES)
+        message(STATUS "Found dynamic USD libraries: ${USD_DYNAMIC_LIBRARIES}")
+    else()
+        message(FATAL_ERROR "Dynamic USD libraries not found")
     endif()
 
     file(GLOB USD_INCLUDE_DIRS ${USD_SEARCH_DIR}/include)
     if(USD_INCLUDE_DIRS)
-        message(STATUS "Found pxr include directory: ${USD_INCLUDE_DIRS}")
+        message(STATUS "Found USD include directory: ${USD_INCLUDE_DIRS}")
         set(USD_INCLUDE_DIR "${USD_SEARCH_DIR}/include")
     else()
-        message(FATAL_ERROR "pxr include folder not found in: ${USD_SEARCH_DIR}/include")
+        message(FATAL_ERROR "USD include folder not found in: ${USD_SEARCH_DIR}/include")
     endif()
 
 else()
     message(FATAL_ERROR "USD base directory not found: ${USD_SEARCH_DIR}")
 endif()
 
-# Set USD_FOUND to true to indicate successful discovery
 set(USD_FOUND TRUE)
 
-# Export the variables for use in the project
-mark_as_advanced(USD_LIBRARIES USD_INCLUDE_DIR)
+mark_as_advanced(USD_STATIC_LIBRARIES USD_DYNAMIC_LIBRARIES USD_INCLUDE_DIR)
